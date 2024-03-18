30c73f9ca02bcc2ed9cf448c33e74a1dc7d6c531
Pvngu <omargt151102@gmail.com>
2024-03-18 06:09:05 -0700

    add hero section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { CardsComponent } from '../../components/cards/cards.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ HeroComponent, CardsComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
